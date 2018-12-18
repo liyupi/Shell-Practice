72209830d96b78858df8dff6b8f0aa9073642b0e
liyupi <592789970@qq.com>
2018-12-18 16:36:10 +0800

    8

---
+++ 8_getsum2.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# get sum of input range
+# easy
+# https://wenku.baidu.com/view/deab4b7b2bf90242a8956bec0975f46527d3a7ad.html 3
+# while operator function $@
+function sum() {
+    n=$1
+    i=1
+    sum=0
+    while [[ i -lt $n ]]; do
+        sum=$((sum+i))
+        i=$((i+1))
+    done
+    echo $sum
+}
+sum $@
+read
